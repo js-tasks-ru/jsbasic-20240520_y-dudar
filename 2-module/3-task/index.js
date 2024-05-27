c0fcbe3e195b69da7250630bdf39f182ea22f92b
Yaroslav Dudar <y.dudar@list.ru>
2024-05-28 01:29:26 +0300

    add 2-module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,27 @@
+// не совсем понял как я это сделал)
+
 let calculator = {
-  // ваш код
+  read: read,
+  sum: sum,
+  mul: mul,
 };
+
+function read(a, b) {
+  calculator.a = a;
+  calculator.b = b;
+}
+
+function sum() {
+  return this.a + this.b;
+}
+
+function mul() {
+  return this.a * this.b;
+}
+
+calculator.read(3, 5);
+calculator.sum();
+calculator.mul();
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
