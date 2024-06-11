e67afc31227f57c1615045b5ea1d90cdc50b0a1e
Yaroslav Dudar <y.dudar@list.ru>
2024-06-11 14:08:57 +0300

    5-module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const button = document.querySelector(".toggle-text-button");
+  button.addEventListener('click', () => {
+    let text = document.getElementById("text");
+    text = text.hasAttribute("hidden") ? text.removeAttribute("hidden") : text.setAttribute("hidden", "true");
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,28 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  let arrowRight = document.querySelector(".carousel__arrow_right");
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+  const carouselMove = document.querySelector(".carousel__inner");
+  const slides = document.querySelectorAll(".carousel__slide");
+  const slideWidth = slides[0].offsetWidth;
+  let currentSlide = 0;
+
+  const arrowUpdates = () => {
+    arrowLeft.style.display = (currentSlide === 0) ? "none" : "";
+    arrowRight.style.display = (currentSlide === slides.length - 1) ? "none" : ""; 
+  };
+  
+  arrowUpdates();
+
+  carousel.addEventListener("click", (event) => {
+    const arrowRightClicked = event.target.closest(".carousel__arrow_right");
+    const arrowLeftClicked = event.target.closest(".carousel__arrow_left");
+    if (arrowRightClicked && currentSlide < slides.length - 1) {
+      currentSlide++;
+    } else if (arrowLeftClicked) {
+      currentSlide--;
+    }
+    carouselMove.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+    arrowUpdates();
+  });
 }
